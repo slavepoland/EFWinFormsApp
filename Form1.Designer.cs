9d858d14568f39a2a7dd17afb4dead7a4e8ab646
slavepoland <wrecz@wp.pl>
2023-09-28 14:02:57 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace EFWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbTable = new System.Windows.Forms.ComboBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbTable
+            // 
+            this.cbTable.FormattingEnabled = true;
+            this.cbTable.Location = new System.Drawing.Point(30, 42);
+            this.cbTable.Name = "cbTable";
+            this.cbTable.Size = new System.Drawing.Size(314, 24);
+            this.cbTable.TabIndex = 0;
+            this.cbTable.SelectedIndexChanged += new System.EventHandler(this.cbTable_SelectedIndexChanged);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(30, 96);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(707, 150);
+            this.dataGrid.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.cbTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbTable;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.Entity.Core.Metadata.Edm;
+
+using System.Collections;
+using System.Runtime.Remoting.Contexts;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace EFWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var metadata = ((IObjectContextAdapter)db).ObjectContext.MetadataWorkspace;
+
+            var tables = metadata.GetItemCollection(DataSpace.SSpace)
+                        .GetItems<EntityContainer>()
+                        .Single()
+                        .BaseEntitySets
+                        .OfType<EntitySet>()
+                        .Where(s => !s.MetadataProperties.Contains("Type")
+                        || s.MetadataProperties["Type"].ToString() == "Tables");
+
+            foreach (var table in tables)
+            {
+                var tableName = table.MetadataProperties.Contains("Table")
+                                && table.MetadataProperties["Table"].Value != null
+                                ? table.MetadataProperties["Table"].Value.ToString()
+                                : table.Name;
+
+                cbTable.Items.Add(table.Name);
+
+            }
+        }
+
+        private void cbTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            var tableName = cb.SelectedItem.ToString();
+            var test = GetAll(tableName);
+
+            dataGrid.DataSource = test;
+
+            /*var table = (IEnumerable)db.GetType().GetProperty(tableName).GetValue(db, null);
+
+
+            List<object> results = new List<object>();
+
+            foreach (var line in table)
+            {
+                //var value = line.GetType().GetProperty().GetValue(line, null);
+
+                //if (value == tableName)
+                //{
+                    results.Add(line);
+                //}
+
+            }
+
+            dataGrid.DataSource = results;*/
+
+
+            /*var table1 = db.GetType()
+            .GetProperty(tableName)
+                           .GetValue(db, null);*/
+
+            //var allResult = db.Employee.Select(x => x).ToList();
+            //dataGrid.DataSource = allResult;
+        }
+
+        public dynamic GetAll(string Table)
+        {
+            var curEntityPI = db.GetType().GetProperty(Table);
+            var curEntityType = curEntityPI.PropertyType.GetGenericArguments().First();
+            // Getting Set<T> method
+            var method = db.GetType().GetMember("Set").Cast<MethodInfo>().Where(x => x.IsGenericMethodDefinition).FirstOrDefault();
+            // Making Set<SomeRealCrmObject>() method
+            var genericMethod = method.MakeGenericMethod(curEntityType);
+            // invoking Setmethod into invokeSet 
+            dynamic invokeSet = genericMethod.Invoke(db, null);
+            // invoking ToList method from Set<> invokeSet 
+            return Enumerable.ToList(invokeSet);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Form form = (Form)sender;
+            dataGrid.Width = Convert.ToInt32(form.Width * 0.9);
+            dataGrid.Height = Convert.ToInt32(form.Height * 0.75);
+        }
+    }
+}
